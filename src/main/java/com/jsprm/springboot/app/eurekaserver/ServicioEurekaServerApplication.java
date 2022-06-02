5f352d4af88208bc222acb39745844340d75c4f3
Jose Alberto Sanchez Pacheco <jasp@ibm.com>
2022-06-02 18:25:28 -0500

    Initial commit

---
+++ src/main/java/com/jsprm/springboot/app/eurekaserver/ServicioEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.jsprm.springboot.app.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicioEurekaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jsprm/springboot/app/eurekaserver/ServicioEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsprm.springboot.app.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
