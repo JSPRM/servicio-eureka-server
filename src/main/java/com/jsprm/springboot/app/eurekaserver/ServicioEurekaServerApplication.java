70a883180fbc920c5e407a059eb3b0322c3b16fd
Jose Alberto Sanchez Pacheco <jasp@ibm.com>
2022-06-02 18:39:07 -0500

    Enable eureka server

--- src/main/java/com/jsprm/springboot/app/eurekaserver/ServicioEurekaServerApplication.java
+++ src/main/java/com/jsprm/springboot/app/eurekaserver/ServicioEurekaServerApplication.java
@@ -2,7 +2,9 @@ package com.jsprm.springboot.app.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ServicioEurekaServerApplication {
 
